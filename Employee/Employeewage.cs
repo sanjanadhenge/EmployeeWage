ce8edeadaeb6d7bb066453f9ec97f0db916395ae
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-04 11:46:12 +0530

    [sanjana] Add : Check attendance

---
+++ Employee/Employeewage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class Employeewage
+    {
+        const int Is_present = 1;
+        public void Attendance()
+        {
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == Is_present)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
