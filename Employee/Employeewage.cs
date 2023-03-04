9f305f4f82d192ff05c5ee759f2f57483754a33f
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-04 14:49:42 +0530

    [sanjana] Add : Calculate wages till a condition of total working hours

--- Employee/Employeewage.cs
+++ Employee/Employeewage.cs
@@ -8,7 +8,7 @@ namespace Employee
 {
     public class Employeewage
     {
-        const int Is_present = 1, Total_working_days = 20,Wage_Per_Hr = 20, Full_Day_Hr = 8, Part_Time_Hr = 4, Is_Full_Time = 0, Is_Part_Time = 1;
+        const int Is_present = 1, Total_working_days = 20,Wage_Per_Hr = 20, Full_Day_Hr = 8, Part_Time_Hr = 4, Is_Full_Time = 0, Is_Part_Time = 1,Total_Working_Hrs = 100;
         public void Attendance()
         {
             Random random = new Random();
@@ -27,7 +27,7 @@ namespace Employee
             int empHrs = 0;
             Random random = new Random();
             int check = random.Next(0, 3);
-            for (int i = 0; i < Total_working_days; i++) 
+            for (int i = 0; i < Total_working_days && empHrs< Total_Working_Hrs; i++) 
             {
                 switch (check)
                 {
