e569ae27c1c5420de5486378b6b59c54c647af1c
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-04 13:03:17 +0530

    [sanjana] Add : Add Part time Emp and Wage

--- Employee/Employeewage.cs
+++ Employee/Employeewage.cs
@@ -8,7 +8,7 @@ namespace Employee
 {
     public class Employeewage
     {
-        const int Is_present = 1, Wage_Per_Hr =20, Full_Day_Hr=8;
+        const int Is_present = 1, Wage_Per_Hr = 20, Full_Day_Hr = 8, Part_Time_Hr = 4, Is_Full_Time = 0, Is_Part_Time = 0;
         public void Attendance()
         {
             Random random = new Random();
@@ -24,8 +24,23 @@ namespace Employee
         }
         public void Dailyempwage()
         {
-            int wage = Wage_Per_Hr * Full_Day_Hr;
-            Console.WriteLine(wage);
+            int empHrs = 0;
+            Random random = new Random();
+            int check = random.Next(0, 3);
+            if (check == Is_Full_Time)
+            {
+                empHrs += Full_Day_Hr;
+            }
+            if (check == Is_Part_Time)
+            {
+                empHrs += Part_Time_Hr;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            int wage = Wage_Per_Hr * empHrs;
+            Console.WriteLine("Daily Wage " + wage);
         }
     }
 }
